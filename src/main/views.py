57904eca4062abfa9c890599ed1ebe61df6a143e
YQtheAnalyst <yiqingli07@gmail.com>
2024-05-25 22:05:48 +0100

    create main page

--- src/main/views.py
+++ src/main/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 
 def main_view(request):
 
-    return HttpResponse("<h1>Welcome to Automax!</h1>")
+    # return HttpResponse("<h1>Welcome to Automax!</h1>")
+    return render(request, "views/main.html", {"name": "Automax"})
 
