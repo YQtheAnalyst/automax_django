c8b107b50bda91a42aa91bd9fd7b1719018bd50b
YQtheAnalyst <yiqingli07@gmail.com>
2024-05-25 13:07:48 +0100

    edit Views

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main', # add main app to this list
 ]
 
 MIDDLEWARE = [
--- src/automax/urls.py
+++ src/automax/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from main import urls as main_app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', include(main_app_urls)) # main/ defines what urls will be used
 ]
