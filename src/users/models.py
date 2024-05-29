ce74b3a7d830f6f8474aed478e2079f8e605f840
YQtheAnalyst <yiqingli07@gmail.com>
2024-05-29 19:47:30 +0100

    add new model - Profile - migrate

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# This model is for users, when they input their information, this model allows them to create a new user
+# account in Django Admin
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(null=True)
+    bio = models.CharField(max_length=140, blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
