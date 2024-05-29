3c2e0aa5b9e7b348903719da40529e9b170ef4a2
YQtheAnalyst <yiqingli07@gmail.com>
2024-05-29 20:06:15 +0100

    register the new model

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from users.models import Profile
+
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)
