4e97539c2e287ee32793ff1004161c701d496349
YQtheAnalyst <yiqingli07@gmail.com>
2024-05-30 20:07:48 +0100

    Add function of adding location in profile

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from users.models import Profile
+from users.models import Profile, Location
 
 # Register your models here.
 
 class ProfileAdmin(admin.ModelAdmin):
     pass
 
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Profile, ProfileAdmin)
+
+admin.site.register(Location, LocationAdmin)
